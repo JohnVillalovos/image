28bc01b490ae743e18d575c5313dc478b2238c2a
Miloslav Trmač <mitr@redhat.com>
2017-01-07 00:18:14 +0100

    WARNING: Dump HTTP requests/responses at logrus Trace level
    WARNING: This includes credentials, if any.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- docker/docker_client.go
+++ docker/docker_client.go
@@ -18,6 +18,7 @@ import (
 	"time"
 
 	"github.com/containers/image/v5/docker/reference"
+	"github.com/containers/image/v5/internal/httpdump"
 	"github.com/containers/image/v5/internal/iolimits"
 	"github.com/containers/image/v5/pkg/docker/config"
 	"github.com/containers/image/v5/pkg/sysregistriesv2"
@@ -546,7 +547,7 @@ func (c *dockerClient) makeRequestToResolvedURLOnce(ctx context.Context, method,
 		}
 	}
 	logrus.Debugf("%s %s", method, url)
-	res, err := c.client.Do(req)
+	res, err := httpdump.DoRequest(c.client, req)
 	if err != nil {
 		return nil, err
 	}
@@ -648,7 +649,7 @@ func (c *dockerClient) getBearerTokenOAuth2(ctx context.Context, challenge chall
 	authReq.Header.Add("User-Agent", c.userAgent)
 	authReq.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	logrus.Debugf("%s %s", authReq.Method, authReq.URL.String())
-	res, err := c.client.Do(authReq)
+	res, err := httpdump.DoRequest(c.client, authReq)
 	if err != nil {
 		return nil, err
 	}
@@ -701,7 +702,7 @@ func (c *dockerClient) getBearerToken(ctx context.Context, challenge challenge,
 	authReq.Header.Add("User-Agent", c.userAgent)
 
 	logrus.Debugf("%s %s", authReq.Method, authReq.URL.String())
-	res, err := c.client.Do(authReq)
+	res, err := httpdump.DoRequest(c.client, authReq)
 	if err != nil {
 		return nil, err
 	}
